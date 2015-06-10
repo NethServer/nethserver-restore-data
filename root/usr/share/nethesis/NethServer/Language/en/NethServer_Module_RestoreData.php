03eae1fea07c65d5c0cb3b7f7cea75234801e1df
Edoardo Spadoni <edoardo.spadoni@nethesis.it>
2015-06-10 09:26:05 +0200

    language. added language translation for restore message. Refs #2773

--- root/usr/share/nethesis/NethServer/Language/en/NethServer_Module_RestoreData.php
+++ root/usr/share/nethesis/NethServer/Language/en/NethServer_Module_RestoreData.php
@@ -5,5 +5,6 @@ $L['RestoreData_Button'] = 'Restore';
 $L['RestoreData_PlaceHolder'] = 'Search dir...';
 $L['RestoreData_Folder_Label'] = 'Directory content';
 $L['RestoreData_original'] = 'Restore data in the original path';
-$L['RestoreData_temp'] = 'Restore data in temporary path';
+$L['RestoreData_temp'] = 'Restore data in new directory';
 $L['RestoreData_String_restore'] = 'Select one or more directories to restore';
+$L['RestoreData_restore_message'] = 'Restored in';
