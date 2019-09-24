430ee85e2aa47daf4644be7541594cf20892c345
Giacomo Sanchietti <giacomo.sanchietti@nethesis.it>
2019-09-24 16:26:46 +0200

    Replace old UI with a placeholder
    Official restore UI is not on Cockpit.

--- root/usr/share/nethesis/NethServer/Module/RestoreData.php
+++ root/usr/share/nethesis/NethServer/Module/RestoreData.php
@@ -1,7 +1,7 @@
 <?php
 /*
- * Copyright (C) 2013 Nethesis S.r.l.
- * http://www.nethesis.it - support@nethesis.it
+ * Copyright (C) 2019 Nethesis S.r.l.
+ * http://www.nethesis.it
  *
  * This script is part of NethServer.
  *
@@ -21,9 +21,8 @@
 
 namespace NethServer\Module;
 use Nethgui\System\PlatformInterface as Validate;
-ini_set('memory_limit', '-1');
 
-class RestoreData extends \Nethgui\Controller\AbstractController implements \Nethgui\Component\DependencyConsumer
+class RestoreData extends \Nethgui\Controller\AbstractController
 {
 
     protected function initializeAttributes(\Nethgui\Module\ModuleAttributesInterface $base)
@@ -31,171 +30,14 @@ class RestoreData extends \Nethgui\Controller\AbstractController implements \Net
         return \Nethgui\Module\SimpleModuleAttributesProvider::extendModuleAttributes($base, 'Configuration', 80);
     }
 
-    // Declare all parameters
-    public function initialize()
-    {
-        parent::initialize();
-        $this->declareParameter('path', Validate::NOTEMPTY);
-        $this->declareParameter('position', Validate::ANYTHING);
-        $this->declareParameter('backupFileList', Validate::ANYTHING);
-    }
-
-    public function process()
-    {
-
-        parent::process();
-        if($this->getRequest()->isMutation()) {
-
-            $path = $this->parameters['path'];
-            file_put_contents('/tmp/TO-restore-file-list', $path);
-            $position = $this->parameters['position'];
-            $program = $this->getPlatform()->getDatabase('backups')->getType('backup-data');
-            if($this->getRequest()->hasParameter('backupFileList') && $program == 'duplicity')
-            {
-                $daysold = (int) floor((time()-$this->getRequest()->getParameter('backupFileList'))/86400);
-            } else {
-                $daysold = 0;
-            }
-            if ($path) {
-                $this->restore_path = $this->getPlatform()->exec('/usr/bin/sudo /usr/libexec/nethserver/nethserver-restore-data-helper ${@}', array($position,$daysold))->getOutput();
-            }
-        }
-    }
-
     public function prepareView(\Nethgui\View\ViewInterface $view) {
-        $db_file = "/var/cache/restore/duc.db";
-        $xml_file = "/var/cache/restore/duc.xml";
-        $start_index = "/";
-        $tree = array();
-        $root = array( 'text' => $start_index, 'children' => array() );
-
         parent::prepareView($view);
-        $bflDatasource = array();
-        $program = $this->getPlatform()->getDatabase('backups')->getType('backup-data');
-        if ($program == 'duplicity') {
-            if ($handle = opendir('/var/cache/restore'))
-            {
-                while (false !== ($entry = readdir($handle))) {
-                    if (preg_match('/duc-([0-9]+)\.xml/', $entry,$tmp)) {
-                        $bflDatasource[] = array($tmp[1],date('j M Y', $tmp[1]));
-                    }
-                }
-                closedir($handle);
-            }
-            rsort($bflDatasource);
+        if (file_exists("/etc/e-smith/db/configuration/defaults/cockpit.socket")) {
+                $view['cockpitUrl'] = "https://". $_SERVER['SERVER_NAME'] . ":9090/nethserver#/applications/nethserver-restore-data";
         } else {
-            $bflDatasource[] = array(1,$view->translate('latest_label'));
+                $view['cockpitUrl'] = '';
         }
-        $view['backupFileListDatasource'] = $bflDatasource;
-        if($this->getRequest()->hasParameter('ts')){
-                $view['backupFileList'] = $this->getRequest()->getParameter('ts');
-        }
-
-        if($this->getRequest()->isMutation() &&  $this->getRequest()->isValidated()) {
-            if(isset($this->restore_path) && $this->restore_path) {
-                $this->notifications->message($view->translate('RestoreData_restore_message', array($this->restore_path)));
-            } else {
-                $this->notifications->message($view->translate('RestoreData_restore_original_message'));
-            }
-        }
-
-        if($this->getRequest()->hasParameter('base')) {
-            if($this->getRequest()->hasParameter('ts')){
-                $xml_file = '/var/cache/restore/duc-'.$this->getRequest()->getParameter('ts').'.xml';
-            }
-            if(!file_exists($xml_file)) {
-                $this->getPlatform()->signalEvent("backup-restore-duc-index");
-            }
-            $xml_string = $this->getPhpWrapper()->file_get_contents($xml_file);
-
-            $xml_full = simplexml_load_string($xml_string);
-
-            foreach (glob("/etc/backup-data.d/*.include") as $filename) {
-                $fileRead = $this->getPhpWrapper()->file_get_contents($filename);
-                $tempArr = explode("\n", $fileRead);
-                foreach ($tempArr as $k) {
-                    $includeArrayDirty[] = $k;
-                }
-            }
-
-            $includeArray = $this->cleanArray($includeArrayDirty);
-
-            foreach (glob("/etc/backup-data.d/*.exclude") as $filename) {
-                $fileRead = $this->getPhpWrapper()->file_get_contents($filename);
-                $tempArr = explode("\n", $fileRead);
-                foreach ($tempArr as $k) {
-                    $excludeArrayDirty[] = $k;
-                }
-            }
-
-            $excludeArray = $this->cleanArray($excludeArrayDirty);
-
-            $alreadyIncluded = array();
-
-            $tree = $this->filter_xml($xml_full, $root, $start_index, $includeArray, $excludeArray, $alreadyIncluded);
-            $view['result'] = $tree;
-        }
-    }
-
-    private function filter_xml($dir, &$root, $start, $includeArray, $excludeArray, $alreadyIncluded) {
-        foreach ($dir->ent as $ent) {
-            $name = trim((string)$ent['name']);
-
-            if($start != '/') {
-                $fullpath = $start."/".$name;
-            } else {
-                $fullpath = "/".$name;
-            }
-
-            if(!in_array($fullpath, $excludeArray)) {
-                if(!in_array($fullpath, $alreadyIncluded)) {
-                    $alreadyIncluded[] = $fullpath;
-                    if ( !$ent->count() ) {
-                        $root['children'][] = array( 'text' => $name, 'type' => trim((string)$ent['type']));
-                    } else {
-                        $node = array( 'text' => $name, 'children' => array() );
-                        $root['children'][] = $this->filter_xml($ent, $node, $fullpath, $includeArray, $excludeArray, $alreadyIncluded);
-                    }
-                }
-            }
-        }
-        return $root;
-    }
-
-    private function cleanArray($arr) {
-        $cleaned = array();
-        foreach ($arr as $k) {
-            if(substr($k, -1) == '/') {
-                $k = substr($k, 0, -1);
-            }
-            $cleaned[] = $k;
-        }
-        return $cleaned;
-    }
-
-    private function walk_dir_clean($dir, &$root) {
-        foreach ($dir->ent as $ent) {
-            $name = trim((string)$ent['name']);
-
-            if ( !$ent->count() ) {
-                $root['children'][] = array( 'text' => $name);
-            } else {
-                $node = array( 'text' => $name, 'children' => array());
-                $root['children'][] = $this->walk_dir_clean($ent, $node);
-            }
-        }
-        return $root;
-    }
-
-    public function setUserNotifications(\Nethgui\Model\UserNotifications $n)
-    {
-        $this->notifications = $n;
-        return $this;
-    }
-
-    public function getDependencySetters()
-    {
-        return array('UserNotifications' => array($this, 'setUserNotifications'));
+        $view['scUrl'] = $view->getModuleUrl('/PackageManager');
     }
 
 }
